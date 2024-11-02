45e1cb28aac374f111991b44b06d04e08c4fc0ae
lenonay <dj.lama.pitito@gmail.com>
2024-11-02 17:08:43 +0000

    Feature: Basic login DB verifying

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,5 +1,15 @@
+import { LocalDB } from "../models/localDB.js";
+
 export class LoginController {
     static async Login(req, res) {
-        res.send(req.body);
+        const { user, passwd } = req.body;
+        // si no hay lo devolvemos
+        if (!user || !passwd) {
+            res.send({ status: "error", error: "Se requiere usuario y contraseña" });
+        }
+
+        const existe = await LocalDB.CheckUser(user, passwd);
+
+        res.send({ status: "OK" , existe: existe});
     }
 }
---
+++ models/localDB.js
@@ -0,0 +1,24 @@
+import { JSONFilePreset } from "lowdb/node";
+import bcrypt from "bcrypt";
+
+const db = await JSONFilePreset("./db.json", { users: [] });
+
+// Si esta vacío metemos uno por defecto
+if (db.data.users.length === 0) {
+    const hash = await bcrypt.hash("123456",10);
+    await db.update(({ users }) => { users.push({ id: 1, user: "nonay", passwd: hash }) });
+}
+
+export class LocalDB {
+    static async CheckUser(user, passwd) {
+        const { users } = db.data;
+
+        const DBuser = users.find((entry) => entry.user === user);
+
+        if(!bcrypt.compareSync(passwd, DBuser.passwd)){
+            return { status: "error", error: "contraseña invalida" }
+        }
+
+        return { status: "OK", erorr: "usuario existente" }
+    }
+}
