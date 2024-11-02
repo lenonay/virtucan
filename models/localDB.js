a9237716c7801cc78cdf23cb610fc81c2ab8feb4
lenonay <dj.lama.pitito@gmail.com>
2024-11-02 17:12:39 +0000

    Feature: Basic login DB verifying

--- models/localDB.js
+++ models/localDB.js
@@ -3,6 +3,11 @@ import bcrypt from "bcrypt";
 
 const db = await JSONFilePreset("./db.json", { users: [] });
 
+if(!db.data.users){
+    db.data.users = [];
+    db.write();
+}
+
 // Si esta vacío metemos uno por defecto
 if (db.data.users.length === 0) {
     const hash = await bcrypt.hash("123456",10);
