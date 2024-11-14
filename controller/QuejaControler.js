fd92d4ed9c28ae42fc74229b41d462bd177b413a
lenonay <dj.lama.pitito@gmail.com>
2024-11-14 18:27:15 +0000

    Feature: files are not deleted after sending img

--- controller/QuejaControler.js
+++ controller/QuejaControler.js
@@ -1,26 +1,9 @@
 import { LocalDB } from "../models/local.js";
-import fs from "node:fs";
 
 export class QuejaController {
     static async register(req, res) {
 
-        const { files } = req.body;
-
-        // IP
-        const ip = req.socket.remoteAddress.split(":").pop();
-
         const response = await LocalDB.register(req.body, ip);
-
-        // Borrar los archivos
-        if (files) {
-            for (const file of files) {
-                const path = process.cwd() + "/uploads/" + file;
-
-                if(fs.existsSync(path)){
-                    fs.unlinkSync(path);
-                }
-            }
-        }
 
         res.send(response);
     }
