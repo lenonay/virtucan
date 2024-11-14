4dd71378336be9ce20789aa787129be3b542595b
lenonay <dj.lama.pitito@gmail.com>
2024-11-14 18:57:21 +0000

    Fix: /files request now get 401 without session

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,9 +1,16 @@
 export function authorize(req, res, next) {
 
+    const URI = req.originalUrl
+
     // Si tiene sesion que continue
     if (req.session) {
         next();
         return
+    }
+
+    if(URI.includes("files")){
+        res.status(401).send("Unauthorized");
+        return;
     }
 
     // Si el metodo es get redirigimos
