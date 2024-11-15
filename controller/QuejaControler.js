354b8fc314de159dd00250b320dbd11133b1e7bc
lenonay <dj.lama.pitito@gmail.com>
2024-11-15 10:13:43 +0000

    Fix: IP has changed

--- controller/QuejaControler.js
+++ controller/QuejaControler.js
@@ -3,7 +3,7 @@ import { LocalDB } from "../models/local.js";
 export class QuejaController {
     static async register(req, res) {
 
-        const response = await LocalDB.register(req.body, ip);
+        const response = await LocalDB.register(req.body);
 
         res.send(response);
     }
